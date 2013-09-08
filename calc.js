1a6d999b6d62827ad1221d4f0cda2b4279ff2d1e
Clark Kampfe <clark.kampfe@gmail.com>
2013-09-08 00:46:56 -0500

    figuring out acessing and changing HTML elements

--- calc.js
+++ calc.js
@@ -6,8 +6,31 @@ function calculate() {
 
 }
 
-
+// EXPERIMENTING TO GET FAMILIAR
 function checker() {
+  // vars are equal to the value in the form field
+  var ap = document.getElementById('apinput').value;
+  var iso = document.getElementById('isoinput').value; 
+  var gn = document.getElementById('gninput').value;
 
+  // elements is an array containing all of the values that are not GN
+  var elements = document.getElementsByName('notgn');
 
+  // write the variable to the specified id. 
+  document.getElementById("apvalue").innerHTML = ap;
+  document.getElementById("isovalue").innerHTML = iso; 
+  document.getElementById("gnvalue").innerHTML = gn;
+  
+  // base text variable
+  var textyText = "";
+  
+  // loop to put each element of the non-gns array in a string, within newline separation
+  for(i=0; i<elements.length; i++) {
+    textyText += (elements[i].id + ": " + elements[i].value + "<br />");
+  } 
+  
+  // write textyText (formerly an array, now a newline separated string) to the testing nongns id element 
+  document.getElementById("nongns").innerHTML = textyText
 }
+
+checker()
