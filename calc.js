f702f52cc738d25d6e12e1407014dae00e8138ee
Clark Kampfe <clark.kampfe@gmail.com>
2013-09-09 19:21:39 -0500

    starting into calc math, added custom sqrt2 function

--- calc.js
+++ calc.js
@@ -1,16 +1,30 @@
+// easier to do logs with a base of SQRT2
+function logSqrt2(val) {
+  return Math.log(val) / Math.log(Math.SQRT2)
+}
+
 function calculateAperture(gn, iso, flashPower, distance) {
-  
+  var isoMod = -(Math.log(iso/100.0,2))
+  var fpMod = Math.log(Math.pow(flashPower, -1.0)) 
+
 }
 
 function calculateDistance(gn, aperture, iso, flashPower) {
+  // transmute ISO, flash power, and aperture into raw stops 
+  var iso_mod = -(Math.log(iso/100.0,2))
+  var fpMod = Math.log(Math.pow(flashPower, -1.0)) 
+  var apMod = logSqrt2(aperture)
+
 
 }
 
 function calculateFlashPower(gn, aperture, iso, distance) {
-
+  var iso_mod = -(Math.log(iso/100.0,2))
+  
 }
 
 function calculateISO(gn, aperture, flashPower, distance) {
+  var fpMod = Math.log(Math.pow(flashPower, -1.0)) 
 
 }
 
